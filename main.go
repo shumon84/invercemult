9c0627b813ba1c889818752c2ac18e8d5c8b677f
shumon84 <cameremon84@gmail.com>
2018-07-16 12:00:06 +0900

    起動時に割り当てるポートを選択できるオプションを追加

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 )
 
 func main() {
+	// オプションをパース
+	var (
+		port string
+		help bool
+	)
+	flag.StringVar(&port, "p", "8080", "割り当てるポート")
+	flag.BoolVar(&help, "h", false, "ヘルプを表示")
+	flag.Parse()
+
+	if help {
+		flag.Usage()
+		return
+	}
+
 	server := http.Server{
-		Addr: ":9000",
+		Addr: ":" + port,
 	}
 	http.HandleFunc("/color", color)
 	http.HandleFunc("/post", post)
